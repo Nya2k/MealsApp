5c95fd35d3e921e2ce6607994d53003cf113f8f9
Nya2k <eudoravanya@gmail.com>
2024-05-25 21:04:40 +0700

    initial commit

---
+++ src/app/navbar.tsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+    return (
+        <div className="text-[#153448] text-xl font-serif font-semibold border-b-2 border-[#153448]">
+            <header className="p-3 flex items-center">
+                <div className='w-1/6 items-center justify-center flex'>
+                    <p className='items-center tracking-tight hover:tracking-wide transition-all w-fit ml-3'>Mom's Recipe</p>
+                </div>
+            </header>
+        </div>
+    )
+}
