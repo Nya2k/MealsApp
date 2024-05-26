e8ce6b2156b1de44aac814e76e6a021226868277
Nya2k <eudoravanya@gmail.com>
2024-05-26 19:47:13 +0700

    add area

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,15 +9,20 @@ interface Category{
   strCategoryDescription: string
 }
 
-interface Breakfast{
-  id : number;
-  strMeal : string;
-  strMealThumb : string;
+interface Area{
+  strArea: string;
 }
+
+// interface Breakfast{
+//   id : number;
+//   strMeal : string;
+//   strMealThumb : string;
+// }
 
 export default function Home() {
   const [categories, setCategories] = useState<Category[]>([]);
-  const [breakfasts, setBreakfast] = useState<Breakfast[]>([]);
+  const [areas, setAreas] = useState<Area[]>([]);
+  // const [breakfasts, setBreakfast] = useState<Breakfast[]>([]);
 
   useEffect(() => {
     axios.get("https:www.themealdb.com/api/json/v1/1/categories.php")
@@ -29,14 +34,24 @@ export default function Home() {
       });
   }, []);
 
-  useEffect (() => {
-    axios.get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Breakfast")
-    .then(res => {
-      setBreakfast(res.data.meals)
-    }).catch(err => {
-      console.log("data breakfast error")
-    })
-  }, [])
+  useEffect(() => {
+    axios.get("https:www.themealdb.com/api/json/v1/1/list.php?a=list")
+      .then(res => {
+        setAreas(res.data.meals);
+      })
+      .catch(err => {
+        console.log("data area error", err);
+      });
+  }, []);
+
+  // useEffect (() => {
+  //   axios.get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Breakfast")
+  //   .then(res => {
+  //     setBreakfast(res.data.meals)
+  //   }).catch(err => {
+  //     console.log("data breakfast error")
+  //   })
+  // }, [])
 
   return (
     <div>
@@ -48,15 +63,15 @@ export default function Home() {
         </div>
       </div>
       {}
-      <div className="flex flex-col justify-center mt-6">
-        <div className="py-2">
-          <h1 className="w-full flex justify-center text-2xl font-serif font-semibold text-[#153448]">CATEGORY</h1>
+      <div className="flex flex-col justify-center mt-4 md:mt-6">
+        <div className="py-2 border-b-2 border-[#948979] border-opacity-30">
+          <h1 className="w-full flex justify-center text-xl md:text-3xl font-serif font-semibold text-[#153448]">RECIPES CATEGORY</h1>
         </div>
         <div className="flex gap-1 md:gap-4 flex-col w-full py-3 px-2 md:px-5">
-        <div className="grid grid-cols-2 gap-1 md:gap-4 flex-col w-full py-3 px-2 md:px-5">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-1 md:gap-4 flex-col w-full py-3 px-2 md:px-5">
           {categories.map((category: any) =>(
-            <div className="bg-[#ffffff] w-full border-2 border-[#948979] rounded shadow-lg p-2 md:p-4 flex flex-col md:flex-row gap-3 items-center hover:bg-[#e9e5de]">
-              <img src={category.strCategoryThumb} alt="beef" className="h-10 md:h-20"/>
+            <div className="bg-[#ffffff] w-full border-2 border-[#948979] rounded shadow-lg p-2 md:p-4 flex flex-col md:flex-row gap-3 items-center hover:bg-[#edeae4] transition-all duration-300">
+              <img src={category.strCategoryThumb} alt={category.strCategory} className="h-10 md:h-20"/>
               <div className="flex flex-col gap-1">
                 <p className="text-base sm:text-lg md:text-xl font-semibold font-serif text-[#153448] text-center md:text-start">{category.strCategory}</p>
                 <p className="text-justify text-xs md:text-sm opacity-95 text-[#3C5B6F]">{category.strCategoryDescription}</p>
@@ -67,7 +82,24 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="flex flex-col justify-center mt-6">
+      <div className="flex flex-col justify-center md:mt-6">
+        <div className="py-2 border-b-2 border-[#948979] border-opacity-30">
+          <h1 className="w-full flex justify-center text-xl md:text-3xl font-serif font-semibold text-[#153448]">RECIPES AREA</h1>
+        </div>
+        <div className="flex gap-1 md:gap-4 flex-col w-full py-1 px-2 md:px-5">
+        <div className="grid grid-cols-2 md:grid-cols-5 gap-1 md:gap-4 flex-col w-full py-3 px-2 md:px-5">
+            {areas.map ((area: any) => (
+              <div className="bg-[#ffffff] w-full border-2 border-[#948979] rounded shadow-lg p-2 md:p-4 flex flex-col md:flex-row gap-3 items-center hover:bg-[#e2ded7] transition-all duration-300">
+                <div className="flex flex-col gap-1">
+                  <p className="text-base sm:text-lg md:text-lg font-medium  text-[#153448] text-center md:text-start">{area.strArea}</p>
+                </div>
+              </div>
+            ))}
+        </div>
+        </div>
+      </div>
+
+      {/* <div className="flex flex-col justify-center mt-6">
         <div className="border-b-2 border-[#948979] border-opacity-30 py-2">
           <h1 className="w-full flex justify-center text-2xl font-serif font-semibold text-[#153448]">BREAKFAST</h1>
         </div>
@@ -79,7 +111,7 @@ export default function Home() {
             </div>
           ))}
         </div>
-      </div>
+      </div> */}
     </div>
   )
 }
