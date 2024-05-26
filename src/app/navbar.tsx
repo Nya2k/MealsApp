de2569a01c288f6ab155cbc842d65cf4f000cf09
Nya2k <eudoravanya@gmail.com>
2024-05-26 16:32:15 +0700

    hero section and breakfast

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,9 +1,9 @@
 export default function Navbar() {
     return (
-        <div className="text-[#153448] text-xl font-serif font-semibold border-b-2 border-[#153448]">
+        <div className="text-[#153448] bg-white text-xl font-serif font-semibold border-b-2 border-[#153448] fixed left-0 top-0 right-0 z-20">
             <header className="p-3 flex items-center">
                 <div className='w-1/6 items-center justify-center flex'>
-                    <p className='items-center tracking-tight hover:tracking-wide transition-all w-fit ml-3'>Mom's Recipe</p>
+                    <p className='items-center w-fit ml-3'>Mom's Recipe</p>
                 </div>
             </header>
         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,49 @@
+'use client'
+import { useState, useEffect } from "react";
+import axios from 'axios';
+
+interface Breakfast{
+  id : number;
+  strMeal : string;
+  strMealThumb : string;
+}
+
 export default function Home() {
+  const [breakfasts, setBreakfast] = useState<Breakfast[]>([]);
+
+  useEffect (() => {
+    axios.get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Breakfast")
+    .then(res => {
+      setBreakfast(res.data.meals)
+    }).catch(err => {
+      console.log("error woi")
+    })
+  }, [])
+
+
   return (
-      <div className="flex flex-col justify-center mt-4 px-5">
-        <div className="border-b-2 border-[#948979] border-opacity-30 py-2">
-          <h1 className="w-full flex justify-center text-2xl font-serif font-semibold text-[#605441]">Breakfast</h1>
-        </div>
-        <div className="flex gap-4 flex-row overflow-x-scroll w-full py-3">
-          <div className="bg-[#ffffff] min-w-56 max-w-64 border-2 border-[#948979] rounded shadow-lg p-4">
-              <img src="https://www.themealdb.com/images/media/meals/ytpstt1511814614.jpg" alt="image" className="rounded"></img>
-              <div className="flex flex-row text-sm opacity-50 mt-1">
-                <p className="w-1/2 text-left">Breakfast</p>
-                <p className="w-1/2 text-right">Russian</p>
-              </div>
-              <p className="w-full text-center mt-3 text-base">Stuffed Bell Peppers with Quinoa and Black Beans</p>
-          </div>
-          
-          
+    <div className="">
+      <div className="relative w-full h-1/2">
+        <img src="/hero-bg.webp" alt="hero-bg" className="object-cover w-full h-full opacity-70"/>
+        <div className="absolute inset-0 flex flex-col items-center justify-center text-center z-10 text-white font-serif bg-black bg-opacity-50 pb-20">
+          <p className="text-7xl font-bold ">Mom’s Recipes</p>
+          <p className="text-4xl tracking-tight hover:tracking-wide transition-all">Heartfelt Meals, Homemade Happiness</p>
         </div>
       </div>
+
+      <div className="flex flex-col justify-center mt-6">
+        <div className="border-b-2 border-[#948979] border-opacity-30 py-2">
+          <h1 className="w-full flex justify-center text-2xl font-serif font-semibold text-[#605441]">BREAKFAST</h1>
+        </div>
+        <div className="flex gap-4 flex-row overflow-x-scroll w-full py-3 px-5">
+          {breakfasts.map((breakfast: any) => (
+            <div className="bg-[#ffffff] min-w-56 max-w-64 border-2 border-[#948979] rounded shadow-lg p-4">
+                <img src={breakfast.strMealThumb} alt="image" className="rounded"></img>
+                <p className="w-full text-center mt-3 text-base">{breakfast.strMeal}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
   )
 }
